3c3122b3262eb9345bbdebbaf0f5559c87e667aa
Ignacio Slater Muñoz <ignacio.slater@ug.uchile.cl>
2020-02-09 18:11:52 -0300

    Next panel's methods v1.0.2.2
    Signed-off-by: islaterm <ignacio.slater@gmail.com>

---
+++ src/main/java/com/github/cc3002/citricjuice/model/board/Panel.java
@@ -0,0 +1,50 @@
+package com.github.cc3002.citricjuice.model.board;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Class that represents a panel in the board of the game.
+ *
+ * @author <a href="mailto:ignacio.slater@ug.uchile.cl">Ignacio Slater Muñoz</a>.
+ * @version 1.0.2.2
+ * @since 1.0
+ */
+public class Panel {
+  private String type;
+  private Set<Panel> nextPanels = new HashSet<>();
+
+  /**
+   * Creates a new panel.
+   *
+   * @param type
+   *     the type of the panel.
+   */
+  public Panel(String type) {
+    this.type = type;
+  }
+
+  /**
+   * @return the type of this panel
+   */
+  public String getType() {
+    return type;
+  }
+
+  /**
+   * @return a copy of this panel's next ones.
+   */
+  public Set<Panel> getNextPanels() {
+    return Set.copyOf(nextPanels);
+  }
+
+  /**
+   * Adds a new adjacent panel to this one.
+   *
+   * @param panel
+   *     the panel to be added.
+   */
+  public void addNextPanel(final Panel panel) {
+    nextPanels.add(panel);
+  }
+}
