d81c66bfcd77c407dddd46ff4cbb269cd53554a9
Tomas Vallejos <tomas.vallejos12@gmail.com>
2020-04-08 22:30:03 -0400

    islaterm: gradle config fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
   id("org.openjfx.javafxplugin") version "0.0.8"
 }
 
+application {
+  mainClassName = "$moduleName/com.github.cc3002.citricliquid.gui.JavaFXHelloWorld"
+}
+
 group = "com.github.cc3002"
 version = "1.0-RELEASE"
 
@@ -11,22 +15,23 @@ repositories {
   mavenCentral()
 }
 
+tasks.named<Test>("test") {
+  useJUnitPlatform()
+}
+
 dependencies {
   implementation(group = "org.openjfx", name = "javafx", version = "14-ea+6", ext = "pom")
   testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api",
-                     version = "5.6.0")}
+          version = "5.1.0")
+  testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine",
+          version = "5.1.0")
+}
 
 javafx {
   version = "14-ea+6"
   modules = mutableListOf("javafx.controls")
 }
-
 configure<JavaPluginConvention> {
   sourceCompatibility = JavaVersion.VERSION_11
 }
 
-tasks {
-  test {
-    useJUnitPlatform()
-  }
-}
