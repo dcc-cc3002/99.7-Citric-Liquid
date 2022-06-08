fbaea60d02b5a45adcb414fa3da8f53a0060cebd
yddolem <105130165+yddolem@users.noreply.github.com>
2022-06-07 23:46:57 -0400

    Update PanelType.java

--- src/main/java/cl/uchile/dcc/citricliquid/model/board/PanelType.java
+++ src/main/java/cl/uchile/dcc/citricliquid/model/board/PanelType.java
@@ -1,12 +1,88 @@
 package cl.uchile.dcc.citricliquid.model.board;
 
-/**
- * Enum for the types of panels of the game.
- *
- * @author <a href="mailto:ignacio.slater@ug.uchile.cl">Ignacio Slater M.</a>.
- * @version 1.1.222804
- * @since 1.0
- */
-public enum PanelType {
-  BONUS, BOSS, DROP, ENCOUNTER, HOME, NEUTRAL
+public class NeutralPanel extends Panel {
+    return
 }
+public class HomePanel extends Panel {
+    public String Owner;
+    public String Jugador;
+    private int normaLevel;
+    normaLevel =Player.getNormaLevel;
+    Jugador =Player.getName();
+
+    private static void Panel.applyHealTo(final @NotNull Player player)
+    private void NormaCheck{
+
+        if (Owner == Jugador) {
+            switch (NormaLevel) {
+                case 1:
+                    if stars == 10;
+                    normaLevel++;
+                    break;
+
+                case 2 -> switch (objetivos) {
+                    case stars:
+                        if stars == 30;
+                        NormaLevel++;
+                        break;
+                    case wins:
+                        if wins == 2;
+                        NormaLevel++;
+                        break;
+                }
+                case 3 -> switch (objetivos) {
+                    case stars:
+                        if stars == 30;
+                        NormaLevel++;
+                        break;
+                    case wins:
+                        if wins == 2;
+                        NormaLevel++;
+                        break;
+                }
+                case 4 -> switch (objetivos) {
+                    case stars:
+                        if stars == 30;
+                        NormaLevel++;
+                        break;
+                    case wins:
+                        if wins == 2;
+                        NormaLevel++;
+                        break;
+                }
+                case 5 -> switch (objetivos) {
+                    case stars:
+                        if stars == 30;
+                        NormaLevel++;
+                        break;
+                    case wins:
+                        if wins == 2;
+                        NormaLevel++;
+                        break;
+                }
+            }
+        }
+    }
+}
+public class BonusPanel extends Panel{
+    private int stars;
+    stars=Player.getstars;
+    int amount = Math.max(roll * NormaLevel, 3 * roll);
+    Player.increaseStarsBy(final int amount)
+}
+
+public class DropPanel extends Panel{
+    private int stars;
+    final int amount = Player.roll;
+    public void reduceStarsBy(final int amount);
+}
+public class DrawPanel extends Panel{
+    system.out.print "aqui se sacan cartas :)"
+}
+public class EncounterPanel extends Panel extends Player {
+    system.out.print "aqui se batalla con una wild unit:)"
+}
+public class BossPanel extends Panel extends Player {
+    system.out.print "aqui batalla con una boss unit :)"
+}
+
